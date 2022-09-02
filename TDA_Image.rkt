d4a279b2653acae99a3881736e47fdc03986d1c3
Iván Guajardo <ivan.guajardo.a@usach.cl>
2022-09-01 23:09:52 -0400

    Primer archivo TDA
    Dejo comentadas las primeras funciones necesarias para poder empezar a implementarlas en los próximos días.

---
+++ TDA_Image.rkt
@@ -0,0 +1,75 @@
+#lang racket
+;; TDA image
+
+
+
+;; image - cons
+;; Nombre:  image
+;; Dominio: Width (int) X Height (int) X [pixbit-d* |  pixrgb-d* |  pixhex-d*]
+;; Recorrido:  image
+;; Descripción: Crea una matriz de ancho width, largo height y de tipo pixbit|pixrgb-d|pixhex-d
+
+
+;;
+;; Nombre: bitmap?
+;; Dominio: image
+;; Recorrido: boolean
+;; Descripción: permite determinar si la imagen corresponde a un bitmap-d
+
+
+;;
+;; Nombre: pixmap?
+;; Dominio: image
+;; Recorrido: boolean
+;; Descripción: permite determinar si la imagen corresponde a un pixmap-d
+
+
+;;
+;; Nombre: hexmap?
+;; Dominio: image
+;; Recorrido: boolean
+;; Descripción: permite determinar si la imagen corresponde a un hexmap-d
+
+;;
+;; Nombre: compressed?
+;; Dominio: image
+;; Recorrido: boolean
+;; Descripción: permite determinar si la imagen está comprimida
+
+
+;;
+;; Nombre: flipH
+;; Dominio: image
+;; Recorrido: image
+;; Descripción: permite invertir una imagen horizontalmente
+
+
+;;
+;; Nombre: flipV
+;; Dominio: image
+;; Recorrido: image
+;; Descripción: permite invertir una imagen verticalmente
+
+
+;;
+;; Nombre: crop
+;; Dominio: image X x1 (int) X y1 (int) X x2 (int) X y2 (int)
+;; Recorrido: image
+;; Descripción: permite recortar una imagen a partir de un cuadrante
+
+
+;;
+;; Nombre: imgRGB->imgHex
+;; Dominio: image
+;; Recorrido: image
+;; Descripción: permite transformar una imagen desde RGB a HEX
+
+
+;; este se debe mover al otro archivo de tda
+;; Nombre: histogram
+;; Dominio: image
+;; Recorrido: histogram
+;; Descripción: crea un histogram y lo muestra
+
+
+;;------------------- Separaré hasta acá mientras tanto -----------------------------------------
