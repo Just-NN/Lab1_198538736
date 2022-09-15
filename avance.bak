dcab475780aa103386327164cec8ffa05e986e75
Ivan Guajardo Arias <ivan.guajardo.a@usach.cl>
2022-09-15 18:45:02 -0300

    Probando flipH
    Intenté implementar flipH sin mucho éxito, sólo logro dar vuelta la imagen por completo

---
+++ avance.bak
@@ -0,0 +1,10 @@
+#lang racket
+
+
+
+(define (image H W . pix)pix)
+
+(define (pixbit-d . pix)
+  (list pix))
+(define (pixbit-d? pix)
+  (if (eq? "pixbit-d" (car (car pix)))#t #f))
--- avance.rkt
+++ avance.rkt
@@ -6,5 +6,26 @@
 
 (define (pixbit-d . pix)
   (list pix))
-(define (pixbit-d? pix)
+
+(define (bitmap? pix)
   (if (eq? "pixbit-d" (car (car pix)))#t #f))
+
+(define (pixmap? pix)
+  (if (eq? "pixbrgb-d" (car (car pix)))#t #f))
+
+(define (hexmap? pix)
+  (if (eq? "pixhex-d" (car (car pix)))#t #f))
+
+(define (lol h l1 l2)
+  (if (= h 0) l2
+      (lol (- h 1) (cdr l1) (cons (car l1) l2))))
+(define (change h w l)
+  (define (change2 h h2 w l aux)
+    (if (= w 0) aux
+        (if (= h 0) (change2 h2 h2 (- w 1) l aux)
+            (change2 (- h 1) h2 w (cdr l) (cons (car l) aux)))))
+  (change2 h h w l '()))
+
+
+
+        
