e5379e23511f74635fcdd489f599acd5bc439e74
Iván Guajardo <ivan.guajardo.a@usach.cl>
2022-09-17 19:29:08 -0300

    Documenté un poquito
    Documenté las funciones principales que llevo hasta ahora. Más adelante planeo documentar todas las que implementé totalmente por mí mismo.

--- TDA_Image.rkt
+++ TDA_Image.rkt
@@ -9,12 +9,20 @@
 
 ; probar con (image 2 2 '("pixbit-d" 0 0 0 1) '("pixbit-d" 1 0 0 1) '("pixbit-d" 0 1 0 1) '("pixbit-d" 1 1 0 1))
 
+
+;; image - constructor
+;; Nombre:  image
+;; Dominio: Width (int) X Height (int) X [pixbit-d* |  pixrgb-d* |  pixhex-d*]
+;; Recorrido:  image
+;; Descripción: Crea una matriz de ancho width, largo height y de tipo pixbit|pixrgb-d|pixhex-d
+
 (define (image Width Height . pix)
   (list Width Height pix))
 
 
 
 ; selectores
+
 (define (get_h img)
   (car img))
 (define (get_w img)
@@ -32,14 +40,40 @@
 
 
 ; predicados
+
+;;
+;; Nombre: bitmap?
+;; Dominio: image
+;; Recorrido: boolean
+;; Descripción: permite determinar si la imagen corresponde a un bitmap-d
+
 (define (bitmap? img)
   (bitmapp? (car (get_pixlist img))))
+
+;;
+;; Nombre: pixmap?
+;; Dominio: image
+;; Recorrido: boolean
+;; Descripción: permite determinar si la imagen corresponde a un pixmap-d
+
 (define (hexmap? img)
   (hexmapp? (car (get_pixlist img))))
+
+;;
+;; Nombre: hexmap?
+;; Dominio: image
+;; Recorrido: boolean
+;; Descripción: permite determinar si la imagen corresponde a un hexmap-d
+
 (define (pixmap? img)
   (pixmapp? (car (get_pixlist img))))
 
-; compressed?
+;;
+;; Nombre: compressed?
+;; Dominio: image
+;; Recorrido: boolean
+;; Descripción: permite determinar si la imagen está comprimida a partir de la cantidad de elementos que
+;; debiera tener según filas*columnas y la que realmente tiene
 
 (define (compressed? p)
   (if (= (* (car p) (cadr p)) (contar (caddr p) 0)) #f #t))
@@ -48,10 +82,11 @@
   (if (null? l) contador
       (contar (cdr l) (+ contador 1))))
 
-; flipH
-
-
-
+;;
+;; Nombre: flipH
+;; Dominio: image
+;; Recorrido: image
+;; Descripción: permite invertir una imagen horizontalmente
 
 (define (flipH img)
   (define (flipHH W W2 pixlist aux)
@@ -59,6 +94,12 @@
         (if (= W -1) (flipHH W2 W2 pixlist aux)
             (flipHH (- W 1) W2 (cdr pixlist) (cons (replace_x (car pixlist) W) aux)))))
     (flipHH (- (get_w img) 1) (- (get_w img) 1) (get_pixlist img) '()))
+
+;;
+;; Nombre: flipV
+;; Dominio: image
+;; Recorrido: image
+;; Descripción: permite invertir una imagen verticalmente
 
 (define (flipV img)
   (define (flipVV H H2 pixlist aux)
@@ -70,10 +111,14 @@
 
 
 ;(flipo 2 (get_pixlist test) '())
-;(flipH test)
+(flipH test)
 (display "------------\n")
 (flipH testhex)
 (display "------------\n")
 (flipH testrgb)
+(display "------------\n")
+(flipV test)
+(display "------------\n")
+(flipV testhex)
 (display "------------\n")
 (flipV testrgb)
